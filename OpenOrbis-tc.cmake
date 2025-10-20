9aac6ed21e2cf9214c17a8145b5744f1cd239525
igor725 <gvaldovigor@gmail.com>
2025-10-20 08:20:21 +0300

    More control over SFO parameters from CMake

--- OpenOrbis-tc.cmake
+++ OpenOrbis-tc.cmake
@@ -34,3 +34,97 @@ if(CMAKE_BUILD_TYPE STREQUAL "Debug")
 else()
   add_compile_options(-O3)
 endif()
+
+function(OpenOrbisPackage_PreProject)
+	if(TARGET ${PKG_TITLE_ID})
+		message(FATAL_ERROR "Test name collision detected: ${PKG_TITLE_ID}.")
+	endif()
+endfunction()
+
+function(OpenOrbisPackage_PostProject)
+	set_target_properties(${PKG_TITLE_ID} PROPERTIES OUTPUT_NAME "eboot" SUFFIX ".elf" PREFIX "")
+
+	if(CMAKE_HOST_WIN32)
+		set(ORBIS_BIN_PATH ${OO_PS4_TOOLCHAIN}/bin/windows)
+	elseif(CMAKE_HOST_LINUX)
+		set(ORBIS_BIN_PATH ${OO_PS4_TOOLCHAIN}/bin/linux)
+	else()
+		message(FATAL_ERROR "Unsupported OS")
+	endif()
+
+  if(OO_PS4_NOPKG)
+    set(PKG_SHOULDBUILD "nopkg")
+  else()
+    set(PKG_SHOULDBUILD "pkg")
+  endif()
+
+	# Create eboot.bin from generated elf file
+	add_custom_command(TARGET ${PKG_TITLE_ID} POST_BUILD COMMAND
+		${CMAKE_COMMAND} -E env "OO_PS4_TOOLCHAIN=${OO_PS4_TOOLCHAIN}"
+		${ORBIS_BIN_PATH}/create-fself -in "${CMAKE_CURRENT_BINARY_DIR}/eboot.elf"
+		--out "${CMAKE_CURRENT_BINARY_DIR}/eboot.oelf" --eboot "${CMAKE_CURRENT_SOURCE_DIR}/eboot.bin" --paid 0x3800000000000011
+	)
+
+	# Create param.sfo and pkg file
+	if(CMAKE_HOST_WIN32)
+		add_custom_command(TARGET ${PKG_TITLE_ID} POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E env "OO_PS4_TOOLCHAIN=${OO_PS4_TOOLCHAIN}"
+			cmd /c ${INTEST_SOURCE_ROOT}/package.bat
+			"${PKG_TITLE}"
+			"${PKG_VERSION}"
+			"${PKG_TITLE_ID}"
+			"${PKG_CONTENT_ID}"
+			"${PKG_DOWNLOADSIZE}"
+			"${PKG_SYSVER}"
+			"${PKG_ATTRIBS1}"
+			"${PKG_ATTRIBS2}"
+			"${PKG_SHOULDBUILD}"
+			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+			COMMENT "Running package.bat..."
+		)
+	elseif(CMAKE_HOST_LINUX)
+		add_custom_command(TARGET ${PKG_TITLE_ID} POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E env "OO_PS4_TOOLCHAIN=${OO_PS4_TOOLCHAIN}"
+			"${INTEST_SOURCE_ROOT}/package.sh"
+			"${PKG_TITLE}"
+			"${PKG_VERSION}"
+			"${PKG_TITLE_ID}"
+			"${PKG_CONTENT_ID}"
+			"${PKG_DOWNLOADSIZE}"
+			"${PKG_SYSVER}"
+			"${PKG_ATTRIBS1}"
+			"${PKG_ATTRIBS2}"
+			"${PKG_SHOULDBUILD}"
+			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+			COMMENT "Running package.sh..."
+		)
+	else()
+		message(FATAL_ERROR "Unsupported OS")
+	endif()
+
+	get_filename_component(CURRENT_FOLDER_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+
+	if(NOT OO_PS4_NOPKG)
+		install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/${PKG_CONTENT_ID}.pkg
+			DESTINATION ${CMAKE_INSTALL_PREFIX}/${CURRENT_FOLDER_NAME})
+	endif()
+
+	# install
+	install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/eboot.bin
+		DESTINATION ${CMAKE_INSTALL_PREFIX}/${CURRENT_FOLDER_NAME})
+
+	install(DIRECTORY
+		${CMAKE_CURRENT_SOURCE_DIR}/sce_sys
+		DESTINATION ${CMAKE_INSTALL_PREFIX}/${CURRENT_FOLDER_NAME}
+	)
+
+	install(DIRECTORY
+		${CMAKE_CURRENT_SOURCE_DIR}/sce_module
+		DESTINATION ${CMAKE_INSTALL_PREFIX}/${CURRENT_FOLDER_NAME}
+	)
+
+	install(DIRECTORY
+		${CMAKE_CURRENT_SOURCE_DIR}/assets
+		DESTINATION ${CMAKE_INSTALL_PREFIX}/${CURRENT_FOLDER_NAME}
+	)
+endfunction()
--- package.bat
+++ package.bat
@@ -11,17 +11,22 @@ set PKG_TITLE=%1
 set PKG_VERSION=%2
 set PKG_TITLE_ID=%3
 set PKG_CONTENT_ID=%4
-set PKG_SHOULDBUILD=%5
+set PKG_DOWNLOADSIZE=%5
+set PKG_SYSVER=%6
+set PKG_ATTRIBS1=%7
+set PKG_ATTRIBS2=%8
+set PKG_SHOULDBUILD=%9
 
 Rem Create param.sfo
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_new sce_sys/param.sfo
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo APP_TYPE --type Integer --maxsize 4 --value 1
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo APP_VER --type Utf8 --maxsize 8 --value %PKG_VERSION%
-%OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo ATTRIBUTE --type Integer --maxsize 4 --value 0
+%OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo ATTRIBUTE --type Integer --maxsize 4 --value %PKG_ATTRIBS1%
+if NOT "%PKG_ATTRIBS2%"=="0" %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo ATTRIBUTE2 --type Integer --maxsize 4 --value %PKG_ATTRIBS2%
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo CATEGORY --type Utf8 --maxsize 4 --value "gd"
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo CONTENT_ID --type Utf8 --maxsize 48 --value %PKG_CONTENT_ID%
-%OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo DOWNLOAD_DATA_SIZE --type Integer --maxsize 4 --value 0
-%OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo SYSTEM_VER --type Integer --maxsize 4 --value 0
+%OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo DOWNLOAD_DATA_SIZE --type Integer --maxsize 4 --value %PKG_DOWNLOADSIZE%
+%OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo SYSTEM_VER --type Integer --maxsize 4 --value %PKG_SYSVER%
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo TITLE --type Utf8 --maxsize 128 --value %PKG_TITLE%
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo TITLE_ID --type Utf8 --maxsize 12 --value %PKG_TITLE_ID%
 %OO_PS4_TOOLCHAIN%\bin\windows\PkgTool.Core.exe sfo_setentry sce_sys/param.sfo VERSION --type Utf8 --maxsize 8 --value %PKG_VERSION%
