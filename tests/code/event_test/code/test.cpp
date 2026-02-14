3377e0cd1e1dd3be525f6d94ad5e79b1a1b04569
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-02-14 15:40:21 -0600

    Event tests
    The start of a rather messy dive into attempting to test most facilities of the PS4, just to see how their events work.

---
+++ tests/code/event_test/code/test.cpp
@@ -0,0 +1,452 @@
+#include "test.h"
+
+#include "CppUTest/TestHarness.h"
+#include "video.h"
+
+#include <bit>
+
+TEST_GROUP (EventTest) {
+  void setup() {}
+  void teardown() {}
+};
+
+static void PrintEventData(OrbisKernelEvent* ev) {
+  printf("ev->ident = 0x%08lx\n", ev->ident);
+  printf("ev->filter = %hd\n", ev->filter);
+  printf("ev->flags = %u\n", ev->flags);
+  printf("ev->fflags = %u\n", ev->fflags);
+  printf("ev->data = 0x%08lx\n", ev->data);
+  if (ev->data != 0) {
+    if (ev->filter == -13) {
+      // Video out event, cast data appropriately.
+      VideoOutEventData data = *reinterpret_cast<VideoOutEventData*>(&ev->data);
+      printf("ev->data->time = %d\n", data.time);
+      printf("ev->data->counter = %d\n", data.counter);
+      printf("ev->data->flip_arg = 0x%08lx\n", data.flip_arg);
+    } else {
+      printf("*(ev->data) = 0x%08lx\n", *(u64*)ev->data);
+    }
+  }
+  printf("ev->user_data = 0x%08lx\n", ev->user_data);
+  if (ev->user_data != 0) {
+    printf("*(ev->user_data) = 0x%08lx\n", *(u64*)ev->user_data);
+  }
+  printf("\n");
+}
+
+static void PrintFlipStatus(OrbisVideoOutFlipStatus* status) {
+  printf("status->count = 0x%08lx\n", status->count);
+  printf("status->process_time = %ld\n", status->process_time);
+  printf("status->tsc_time = %ld\n", status->tsc_time);
+  printf("status->flip_arg = 0x%08lx\n", status->flip_arg);
+  printf("status->submit_tsc = %ld\n", status->submit_tsc);
+  printf("status->num_gpu_flip_pending = %d\n", status->num_gpu_flip_pending);
+  printf("status->num_flip_pending = %d\n", status->num_flip_pending);
+  printf("status->current_buffer = %d\n\n", status->current_buffer);
+}
+
+TEST(EventTest, Test) {
+  // Need to test some equeue behavior.
+  // Start by creating an equeue.
+  OrbisKernelEqueue eq {};
+  s32               result = sceKernelCreateEqueue(&eq, "TestEqueue");
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Add a user event to this equeue, use id 32.
+  result = sceKernelAddUserEvent(eq, 32);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Trigger the event
+  u64 data1 = 100;
+  result    = sceKernelTriggerUserEvent(eq, 32, &data1);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  OrbisKernelEvent ev {};
+  s32              count {};
+  result = sceKernelWaitEqueue(eq, &ev, 1, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(0, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(100, *(u64*)ev.user_data);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  // Since user events don't clear, this will return the data from the first trigger.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = sceKernelWaitEqueue(eq, &ev, 1, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(0, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(100, *(u64*)ev.user_data);
+
+  // Re-trigger the event, this should update userdata
+  u64 data2 = 200;
+  result    = sceKernelTriggerUserEvent(eq, 32, &data2);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = sceKernelWaitEqueue(eq, &ev, 1, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(0, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(200, *(u64*)ev.user_data);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  memset(&ev, 0, sizeof(ev));
+  count = 0;
+  // Succeeds, but only returns 1 event, since there's only one triggered event to return.
+  result = sceKernelWaitEqueue(eq, &ev, 2, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(0, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(200, *(u64*)ev.user_data);
+
+  // Delete the user event.
+  result = sceKernelDeleteUserEvent(eq, 32);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Add a "user event edge", these are user events with the clear flag.
+  // Presumably, these will not trigger multiple times.
+  // Add a user event to this equeue, use id 32.
+  result = sceKernelAddUserEventEdge(eq, 32);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Trigger the event
+  result = sceKernelTriggerUserEvent(eq, 32, &data1);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = sceKernelWaitEqueue(eq, &ev, 1, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(100, *(u64*)ev.user_data);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  // Due to the clear flag, this should time out.
+  memset(&ev, 0, sizeof(ev));
+  count       = 0;
+  u32 timeout = 100;
+  result      = sceKernelWaitEqueue(eq, &ev, 1, &count, &timeout);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_ETIMEDOUT, result);
+
+  // Re-trigger the event, this should update userdata
+  result = sceKernelTriggerUserEvent(eq, 32, &data2);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = sceKernelWaitEqueue(eq, &ev, 1, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(200, *(u64*)ev.user_data);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  memset(&ev, 0, sizeof(ev));
+  count   = 0;
+  timeout = 100;
+  // Fails, since there isn't any event to return.
+  result = sceKernelWaitEqueue(eq, &ev, 2, &count, &timeout);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_ETIMEDOUT, result);
+
+  // Trigger the event twice, check behavior.
+  result = sceKernelTriggerUserEvent(eq, 32, &data1);
+  UNSIGNED_INT_EQUALS(0, result);
+  result = sceKernelTriggerUserEvent(eq, 32, &data2);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Run sceKernelWaitEqueue to detect the returned event.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = sceKernelWaitEqueue(eq, &ev, 1, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  CHECK_EQUAL(32, ev.ident);
+  CHECK_EQUAL(-11, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data == 0);
+  CHECK(ev.user_data != 0);
+  CHECK_EQUAL(200, *(u64*)ev.user_data);
+
+  // Even though we trigger twice, this call will fail.
+  memset(&ev, 0, sizeof(ev));
+  count   = 0;
+  timeout = 100;
+  result  = sceKernelWaitEqueue(eq, &ev, 1, &count, &timeout);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_ETIMEDOUT, result);
+
+  // Delete the user event.
+  result = sceKernelDeleteUserEvent(eq, 32);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Delete the equeue when tests complete.
+  result = sceKernelDeleteEqueue(eq);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Test video out flip events.
+  // First we need to properly open libSceVideoOut
+  VideoOut* handle = new VideoOut(1920, 1080);
+
+  // Register buffers
+  handle->addBuffer();
+  handle->addBuffer();
+  handle->addBuffer();
+
+  OrbisVideoOutFlipStatus status {};
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Create a flip event
+  result = handle->addFlipEvent(nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Not sure what we're dealing with, so from here, start logging info.
+  PrintFlipStatus(&status);
+
+  // Perform a flip
+  result = handle->flipFrame(0x100);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Now we can wait on the flip event equeue prepared earlier.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = handle->waitFlipEvent(&ev, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  // CHECK_EQUAL(0x6000000000000, ev.ident);
+  CHECK_EQUAL(-13, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data != 0);
+  VideoOutEventData ev_data = *reinterpret_cast<VideoOutEventData*>(&ev.data);
+  CHECK_EQUAL(1, ev_data.counter);
+  CHECK_EQUAL(0x100, ev_data.flip_arg);
+
+  // Flip events only trigger once.
+  memset(&ev, 0, sizeof(ev));
+  count   = 0;
+  timeout = 1000;
+  result  = handle->waitFlipEvent(&ev, &count, &timeout);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_ETIMEDOUT, result);
+
+  // Check flip status
+  memset(&status, 0, sizeof(status));
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  PrintFlipStatus(&status);
+
+  // Perform a flip
+  result = handle->flipFrame(0x200);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Now we can wait on the flip event equeue prepared earlier.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = handle->waitFlipEvent(&ev, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  // CHECK_EQUAL(0x6000000000000, ev.ident);
+  CHECK_EQUAL(-13, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data != 0);
+  ev_data = *reinterpret_cast<VideoOutEventData*>(&ev.data);
+  CHECK_EQUAL(1, ev_data.counter);
+  CHECK_EQUAL(0x200, ev_data.flip_arg);
+
+  // Flip events only trigger once.
+  memset(&ev, 0, sizeof(ev));
+  count   = 0;
+  timeout = 1000;
+  result  = handle->waitFlipEvent(&ev, &count, &timeout);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_ETIMEDOUT, result);
+
+  // Check flip status
+  memset(&status, 0, sizeof(status));
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  PrintFlipStatus(&status);
+
+  // Fire two video out events in quick succession
+  result = handle->flipFrame(0x100);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+  PrintFlipStatus(&status);
+
+  result = handle->flipFrame(0x300);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+  PrintFlipStatus(&status);
+
+  // Now we can wait on the flip event equeue prepared earlier.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = handle->waitFlipEvent(&ev, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  // CHECK_EQUAL(0x6000000000000, ev.ident);
+  CHECK_EQUAL(-13, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data != 0);
+  ev_data = *reinterpret_cast<VideoOutEventData*>(&ev.data);
+  CHECK_EQUAL(1, ev_data.counter);
+  CHECK_EQUAL(0x100, ev_data.flip_arg);
+
+  // Check flip status
+  memset(&status, 0, sizeof(status));
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  PrintFlipStatus(&status);
+
+  // We did two submits, so the video out event should fire again.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = handle->waitFlipEvent(&ev, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  // Check returned data
+  PrintEventData(&ev);
+  // CHECK_EQUAL(0x6000000000000, ev.ident);
+  CHECK_EQUAL(-13, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data != 0);
+  ev_data = *reinterpret_cast<VideoOutEventData*>(&ev.data);
+  CHECK_EQUAL(1, ev_data.counter);
+  CHECK_EQUAL(0x300, ev_data.flip_arg);
+
+  // Check flip status
+  memset(&status, 0, sizeof(status));
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  PrintFlipStatus(&status);
+
+  // Fire two video out events in quick succession
+  result = handle->flipFrame(0x400);
+  UNSIGNED_INT_EQUALS(0, result);
+  result = handle->flipFrame(0x500);
+  UNSIGNED_INT_EQUALS(0, result);
+
+  // Use sceVideoOutGetFlipStatus to wait for both flips to complete
+  memset(&status, 0, sizeof(status));
+  result = handle->getStatus(&status);
+  UNSIGNED_INT_EQUALS(0, result);
+  while (status.num_flip_pending != 0) {
+    sceKernelUsleep(1000);
+
+    memset(&status, 0, sizeof(status));
+    result = handle->getStatus(&status);
+    UNSIGNED_INT_EQUALS(0, result);
+  }
+
+  // Both flips are done, check status and event
+  PrintFlipStatus(&status);
+
+  // Check returned data
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = handle->waitFlipEvent(&ev, &count, nullptr);
+  UNSIGNED_INT_EQUALS(0, result);
+  CHECK_EQUAL(1, count);
+
+  PrintEventData(&ev);
+  // CHECK_EQUAL(0x6000000000000, ev.ident);
+  CHECK_EQUAL(-13, ev.filter);
+  CHECK_EQUAL(32, ev.flags);
+  CHECK_EQUAL(0, ev.fflags);
+  CHECK(ev.data != 0);
+  ev_data = *reinterpret_cast<VideoOutEventData*>(&ev.data);
+  // Counter is how many times the event was triggered.
+  CHECK_EQUAL(2, ev_data.counter);
+  CHECK_EQUAL(0x500, ev_data.flip_arg);
+
+  // Shouldn't trigger again.
+  memset(&ev, 0, sizeof(ev));
+  count  = 0;
+  result = handle->waitFlipEvent(&ev, &count, &timeout);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_ETIMEDOUT, result);
+
+  // Now for the slightly messy part, we need to test EOP flips.
+  // These are GPU driven flips, which work alongside the CPU driven flips tested earlier.
+
+  // Clean up after test
+  delete (handle);
+}
---
+++ tests/code/event_test/code/video.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "test.h"
+
+#include <vector>
+
+class VideoOut {
+  private:
+  s32                              handle {0};
+  s32                              width {0};
+  s32                              height {0};
+  std::vector<std::pair<s64, u64>> phys_bufs {};
+  OrbisKernelEqueue                flip_queue {nullptr};
+  bool flip_event = false;
+  s32 current_buf = -1;
+
+  public:
+  VideoOut(s32 width, s32 height) {
+    this->width  = width;
+    this->height = height;
+    // Open VideoOut handle
+    this->handle = sceVideoOutOpen(255, 0, 0, nullptr);
+    // Set flip rate to 60fps
+    s32 result = sceVideoOutSetFlipRate(this->handle, 0);
+    UNSIGNED_INT_EQUALS(0, result);
+    // Create flip equeue
+    result = sceKernelCreateEqueue(&flip_queue, "VideoOutFlipEqueue");
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  // Manually define a destructor to close everything.
+  ~VideoOut() {
+    if (flip_event) {
+      s32 result = sceVideoOutDeleteFlipEvent(flip_queue, handle);
+      UNSIGNED_INT_EQUALS(0, result);
+      flip_event = false;
+    }
+    if (flip_queue != nullptr) {
+      s32 result = sceKernelDeleteEqueue(flip_queue);
+      UNSIGNED_INT_EQUALS(0, result);
+    }
+    if (handle > 0) {
+      s32 result = sceVideoOutClose(handle);
+      UNSIGNED_INT_EQUALS(0, result);
+      handle = 0;
+    }
+    for (auto& [phys_off, size]: phys_bufs) {
+      s32 result = sceKernelReleaseDirectMemory(phys_off, size);
+      UNSIGNED_INT_EQUALS(0, result);
+    }
+    phys_bufs.~vector();
+  }
+
+  s32 getStatus(OrbisVideoOutFlipStatus* status) {
+    return sceVideoOutGetFlipStatus(handle, status);
+  }
+
+  s32 flipFrame(s64 flip_arg) {
+    s32 result = sceVideoOutSubmitFlip(handle, current_buf, 1, flip_arg);
+    if (++current_buf == phys_bufs.size()) {
+      current_buf = -1;
+    }
+    return result;
+  }
+
+  s32 addBuffer() {
+    // Create a buffer attribute
+    OrbisVideoOutBufferAttribute attr {};
+    attr.pixel_format   = 0x80002200;
+    attr.tiling_mode    = 0;
+    attr.aspect_ratio   = 0;
+    attr.width          = width;
+    attr.height         = height;
+    attr.pitch_in_pixel = width;
+    attr.option         = 0;
+    attr.reserved0      = 0;
+    attr.reserved1      = 0;
+
+    // calc some stuff, logic taken from red_prig's shader test homebrew.
+    u64 pitch        = (attr.width + 127) / 128;
+    u64 pad_width    = pitch * 128;
+    u64 pad_height   = ((attr.height + 63) & (~63));
+    u64 size         = pad_width * pad_height * 4;
+    u64 aligned_size = (size + 16 * 1024 - 1) & ~(16 * 1024 - 1);
+
+    // Map memory for buffer.
+    s64 dmem_addr = 0;
+    s32 result    = sceKernelAllocateMainDirectMemory(aligned_size, 64 * 1024, 3, &dmem_addr);
+    UNSIGNED_INT_EQUALS(0, result);
+    void* addr = nullptr;
+    result     = sceKernelMapDirectMemory(&addr, aligned_size, 0x33, 0, dmem_addr, 64 * 1024);
+    UNSIGNED_INT_EQUALS(0, result);
+    memset(addr, 0, aligned_size);
+
+    // Add buffer info to the buffers list
+    phys_bufs.emplace_back(dmem_addr, aligned_size);
+
+    // Register buffer
+    return sceVideoOutRegisterBuffers(handle, phys_bufs.size() - 1, &addr, 1, &attr);
+  };
+
+  s32 addFlipEvent(void* user_data) {
+    s32 result = sceVideoOutAddFlipEvent(flip_queue, handle, user_data);
+    flip_event = true;
+    return result;
+  };
+
+  s32 deleteFlipEvent() {
+    s32 result = sceVideoOutDeleteFlipEvent(flip_queue, handle);
+    flip_event = false;
+    return result;
+  };
+
+  s32 waitFlipEvent(OrbisKernelEvent* ev, s32* out, u32* timeout) {
+    return sceKernelWaitEqueue(flip_queue, ev, 1, out, timeout);
+  };
+};
